f510f5373faa558acf1db1f98ce5b210031f1cac
dmitriys9701 <dmitriys9701@yandex.ru>
2019-02-22 20:28:47 +0300

    fix bg set

--- scripts/thinkpad-docking.sh
+++ scripts/thinkpad-docking.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export DISPLAY=:0
 export XAUTHORITY=/home/nland/.Xauthority
-sleep 2 && xrandr --output DP-2-3 --primary --mode 1920x1080 --output eDP-1 --mode 1920x1080 --left-of DP-2-3
+sleep 2 && xrandr --output DP-2-3 --primary --mode 1920x1080 --output eDP-1 --mode 1920x1080 --below DP-2-3 && ~/.fehbg
